a65bbdd8aa2e12660973a27e89e3989b4130eb61
Jared Forth <jaredforthdev@gmail.com>
2020-10-13 16:39:43 -0500

    update metadata

--- src/lib.rs
+++ src/lib.rs
@@ -1,3 +1,13 @@
+// Copyright 2020 Jared Forth.
+//
+// Licensed under the Apache License, Version 2.0 <LICENSE-APACHE or
+// https://www.apache.org/licenses/LICENSE-2.0> or the MIT license
+// <LICENSE-MIT or https://opensource.org/licenses/MIT>, at your
+// option. This file may not be copied, modified, or distributed
+// except according to those terms.
+
+//! **roy** is a high-level library for consuming RESTful APIs.
+
 use reqwest::{Response, header};
 
 /// The HTTP client
